672148ba8a35519cbae69abdab3479cbdeb72eef
SabinTandukar <sabn.tndkr22@gmail.com>
2026-02-24 21:13:51 +0545

    added registration and login functionality

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,57 @@
+// create context
+import { createContext, useState } from "react";
+
+export const AuthContext = createContext(null);
+
+// AuthProvider is a wrapper component
+export default function AuthProvider({ children }) {
+  const [user, setUser] = useState(
+    localStorage.getItem("currentUserEmail")
+      ? { email: localStorage.getItem("currentUserEmail") }
+      : null,
+  );
+
+  function signUp(email, password) {
+    // insert users in local storage
+    const users = JSON.parse(localStorage.getItem("users") || "[]"); //checks if email and password
+
+    // check if user is already signed up
+    if (users.find((u) => u.email === email)) {
+      return { success: false, error: "Email already exists" };
+    }
+    const newUser = { email, password }; //takes email and password from the user
+    users.push(newUser); //inserts the email and passwod in the empty array
+    localStorage.setItem("users", JSON.stringify(users)); //sets email and password in localstorage
+    localStorage.setItem("currentUserEmail", email); //save users email
+
+    setUser({ email });
+    return { success: true };
+  }
+
+  function logIn(email, password) {
+    const users = JSON.parse(localStorage.getItem("users") || "[]");
+    const user = users.find(
+      (u) => u.email === email && u.password === password,
+    );
+
+    if (!user) {
+      return { success: false, error: "Invalid email or password" };
+    }
+
+    localStorage.setItem("currentUserEmail", email);
+    setUser({ email });
+
+    return { success: true };
+  }
+
+  //   logging out
+  function logOut() {
+    localStorage.removeItem("currenUserEmail");
+    setUser(null);
+  }
+  return (
+    <AuthContext.Provider value={{ signUp, user, logOut, logIn }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,3 +1,108 @@
+import { useContext, useState } from "react";
+import { useForm } from "react-hook-form";
+import { AuthContext } from "../context/AuthContext";
+
 export default function Auth() {
-  return <h1>Auth Page</h1>;
+  const [mode, setMode] = useState("signup");
+  const [error, setError] = useState(null);
+  const { signUp, user, logOut, logIn } = useContext(AuthContext);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  function onSubmit(data) {
+    setError(null);
+    let result;
+    if (mode === "signup") {
+      result = signUp(data.email, data.password);
+    } else {
+      result = logIn(data.email, data.password);
+    }
+
+    if (result.success) {
+      alert("Yey");
+    } else {
+      setError(result.error);
+    }
+  }
+
+  return (
+    <div className="page">
+      <div className="container">
+        <div className="auth-container">
+          {user && <p>User loged in {user.email}</p>}
+          <button onClick={() => logOut()}>LogOut</button>
+          <h1 className="page-title">
+            {mode === "signup" ? "Sign Up" : "Login"}
+          </h1>
+          <form className="auth-form" onSubmit={handleSubmit(onSubmit)}>
+            {error && <div className="error-message">{error}</div>}
+            <div className="form-group">
+              <label htmlFor="email" className="form-label">
+                Email
+              </label>
+              <input
+                type="email"
+                className="form-input"
+                id="email"
+                {...register("email", { required: "Email is required" })}
+              />
+              {errors.email && (
+                <span className="form-error">{errors.email.message}</span>
+              )}
+            </div>
+            <div className="form-group">
+              <label htmlFor="password" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                className="form-input"
+                id="password"
+                {...register("password", {
+                  required: "Password is required",
+                  minLength: {
+                    value: 6,
+                    message: "Password must be at least 6 characters",
+                  },
+                  maxLength: {
+                    value: 12,
+                    message: "Password must be less than 12 characters",
+                  },
+                })}
+              />
+              {errors.password && (
+                <span className="form-error">{errors.password.message}</span>
+              )}
+            </div>
+
+            <button type="submit" className="btn btn-primary btn-large">
+              {mode === "signup" ? "Sign Up" : "Login"}
+            </button>
+          </form>
+
+          <div className="auth-switch">
+            {mode === "signup" ? (
+              <p>
+                Already have and account?{" "}
+                <span className="auth-link" onClick={() => setMode("login")}>
+                  Login
+                </span>
+              </p>
+            ) : (
+              <p>
+                Don't have and account?{" "}
+                <span className="auth-link" onClick={() => setMode("signup")}>
+                  Signup
+                </span>
+              </p>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
